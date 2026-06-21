8356a5f2b413f0cec947efbc3be94591394435c6
BougieZoe <mianwooli@gmail.com>
2026-06-21 20:19:53 +0800

    add vercel analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import { ThemeProvider } from "@/lib/ThemeContext";
 import "./index.css";
@@ -6,5 +7,6 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider initialTheme="dark">
     <App />
+    <Analytics />
   </ThemeProvider>
 );
