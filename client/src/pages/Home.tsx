61896a6f487158bf10b01e929f8707f93951b303
BougieZoe <mianwooli@gmail.com>
2026-08-25 21:53:37 +0800

    wip: move buttons to bottom-right below voice control

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1027,9 +1027,9 @@ deepAnalysisSeq.current += 1;
               t={t}
             />
           )}
-          {/* Feature buttons - bottom left of viewport */}
+          {/* Feature buttons - bottom right, below voice control */}
           {uploadedModel && (
-            <div className="absolute bottom-4 left-4 flex flex-row gap-2 z-20">
+            <div className="absolute bottom-24 right-6 flex flex-row gap-2 z-20">
               <button
                 onClick={() => setShowPrintDashboard(true)}
                 className="w-10 h-10 rounded-full bg-cyan-500/20 border border-cyan-500/40 text-cyan-400 flex items-center justify-center hover:bg-cyan-500/30 transition-colors"
